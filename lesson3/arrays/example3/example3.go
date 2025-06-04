9ff4bd354db635dc6531c8e611e278b0c09a8446
gkjoyes <georgekuttyjoyes@gmail.com>
2025-06-04 21:49:58 +0530

    Add example for contiguos memory allocation

---
+++ lesson3/arrays/example3/example3.go
@@ -0,0 +1,12 @@
+// Sample program to show memeory for an array is contiguous.
+package main
+
+import "fmt"
+
+func main() {
+	users := [4]string{"George", "Sruthy", "Jishnu", "Tony"}
+
+	for i, u := range users {
+		fmt.Printf("Value: [%v]\tAddress: [%p]\tIndexAddress: [%p]\n", u, &u, &users[i])
+	}
+}
